b9d5f3d760ff82e2ad9cc022238b49fda7b3dd58
kritartha_kc <kritartha@Kritarthas-iMac.local>
2022-11-17 12:53:42 +0545

    a

---
+++ src/components/AddContact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AddContact = () => {
+  return (
+    <div className='Ui main'>
+      <h2>Add Contact</h2>
+      <form className='ui form'>
+        <div className='field'>
+            <label>Name</label>
+            <input type="text" name='name' placeholder='Name'/>
+
+            <div className='field'>
+            <label>Email</label>
+            <input type="text" name='name' placeholder='Email'/>
+            </div>
+        </div>
+        <button className='ui button red'>Add </button>
+         </form>
+    </div>
+  )
+}
+
+export default AddContact
